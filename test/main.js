e8a3c97702362759b7fedb7af3a0923e08120791
Jesse Harlin <harlinjesse@gmail.com>
2015-03-29 23:58:03 -0500

    updated the ci for this little project

--- test/main.js
+++ test/main.js
@@ -1,3 +1,12 @@
 'use strict'
 
 var webpack = require('webpack');
+
+
+describe('Given a phaser-glsl-loader', function() {
+  describe('When I provide a valid filepath', function() {
+    it('Should convert the glsl file into a format consumable by phaser', function() {
+
+    });
+  });
+});
