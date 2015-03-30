edef1d6a9d062492cfc3993808a6f251a2ebc8bb
Jesse Harlin <harlinjesse@gmail.com>
2015-03-29 23:25:46 -0500

    created files, things load

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var _ = require('lodash');
+
+/** gl-loader */
+function toCleanStrings(line) {
+  return _.trim(line);
+}
+
+function emptyStuff(line) {
+  if (line) {
+    return line;
+  }
+}
+
+function toStrings(line) {
+  return '"' + line + '"';
+}
+
+function glFragmentLoader(source) {
+  this.cacheable && this.cacheable();
+
+  var sourceArray = source.split('\n');
+
+  var cleanData = _(sourceArray)
+    .map(toCleanStrings)
+    .filter(emptyStuff)
+    .map(toStrings)
+    .value();
+
+  return 'module.exports=[' + cleanData + '];';
+}
+
+module.exports = glFragmentLoader;
